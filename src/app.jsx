b5dde757404245ffe252ccc2233ac3bfa55292dd
Alec Stanton <alecstanton@Alecs-MacBook-Pro.local>
2018-01-01 23:48:54 -0800

    table data

--- src/app.jsx
+++ src/app.jsx
@@ -8,55 +8,76 @@ import "react-table/react-table.css";
 import "./css/bootstrap/css/bootstrap.min.css";
 import "./css/style.css";
 import io from "socket.io-client";
-// import { getPrices } from "./utils/api";
 
 const data = [];
 const columns = [
     { Header: "#", accessor: "#" },
     { Header: "Coin", accessor: "coin" },
-    { Header: "Ticker", accessor: "ticker" },
+    { Header: "Ticker", accessor: "product_id" },
     { Header: "Price", accessor: "price" },
-    { Header: "% Change", accessor: "percentChange" },
-    { Header: "Volume", accessor: "column" },
+    { Header: "Day % Change", accessor: "percent_change" },
+    { Header: "Day High", accessor: "high" },
+    { Header: "Day Low", accessor: "low" },
+    { Header: "Volume", accessor: "volume" },
     { Header: "Circulating Supply", accessor: "circulationSupply" }
 ];
 const socket = io("http://localhost:8080");
 
 class App extends React.Component {
-    state = { data: [], binance_data: null };
+    state = { 
+        data: { 
+            "BTC": { coin: 'Bitcoin', product_id: "BTC" },
+            "ETH": { coin: 'Ethereum', product_id: "ETH" },
+            "LTC": { coin: 'Litecoin', product_id: "LTC" },
+            "BCH": { coin: 'Bitcoincash', product_id: "BCH" },
+            "ETC": { coin: 'Ethereumclassic', product_id: "ETC" },
+            "RRT": { coin: 'Recovery Right Tokens', product_id: "RRT" },
+            "ZEC": { coin: 'Zcash', product_id: "ZEC" },
+            "XMR": { coin: 'Monero', product_id: "XMR" },
+            "DSH": { coin: 'Dashcoin', product_id: "DSH" },
+            "XRP": { coin: 'Ripple', product_id: "XRP" },
+            "IOT": { coin: 'Iota', product_id: "IOT" },
+            "EOS": { coin: 'Eos', product_id: "EOS" },
+            "SAN": { coin: 'Santiment', product_id: "SAN" },
+            "OMG": { coin: 'Omisego', product_id: "OMG" },
+            "NEO": { coin: 'Neo', product_id: "NEO" },
+            "ETP": { coin: 'Metaverse ETP', product_id: "ETP" },
+            "QTM": { coin: 'Qtum', product_id: "QTM" },
+            "AVT": { coin: 'Aventus', product_id: "AVT" },
+            "EDO": { coin: 'Eidoo', product_id: "EDO" },
+            "BTG": { coin: 'Bitcoin Gold', product_id: "BTG" },
+            "DAT": { coin: 'Datum', product_id: "DAT" },
+            "QSH": { coin: 'QASH', product_id: "QSH" },
+            "YYW": { coin: 'YOYOW', product_id: "YYW" },
+            "GNT": { coin: 'Golem', product_id: "GNT" },
+            "SNT": { coin: 'Status', product_id: "SNT" }
+        },
+        last_update: JSON.stringify(new Date())
+    };
 
     async componentDidMount() {
         socket.on("gdax_feed", message => {
             this.handleDataFeed(message);
         });
 
-        socket.on("coinbase_api", message => {
-            this.setState({
-                coinbase_data: message
-            });
-        });
-
-        socket.on("binance_data", message => {
-            console.log("got updated binance data...");
-            this.setState({
-                binance_data: message
-            });
+        socket.on("bfx_feed", message => {
+            this.handleDataFeed(message);
         });
     }
 
-    handleDataFeed = async event => {
+    handleDataFeed = event => {
+        let data = {};
+        Object.entries(this.state.data).forEach(([key, value]) => {
+            data[key] = { ...value, ...event[key] };
+        });
         this.setState({
-            data: [...event]
+            data: data,
+            last_update: JSON.stringify(new Date())
         });
     };
 
     render() {
-        const coins = this.state.data.map(coin => (
-            <li key={`${coin.sequence}`}>
-                {coin.product_id} --- {coin.side} ---- {coin.price}
-            </li>
-        ));
-        const length = coins.length;
+        const data = Object.entries(this.state.data).map(([key, value]) => value);
         return (
             <div className="App">
                 <div className="Header">
@@ -67,19 +88,10 @@ class App extends React.Component {
                 </h1>
                 <div className="container">
                     <SubNav />
+                    <div>
+                        <p>{this.state.last_update}</p>
+                    </div>
                     <ReactTable data={data} columns={columns} />
-                </div>
-
-                <div>
-                    <br />
-                    Last Updated: {`${new Date()}`}
-                    <br />
-                    Length of Binance Feed: {length}
-                    <br />
-                    {this.state.binance_data}
-                    {/* <ul>{coins}</ul> */}
-                    <br />
-                    Length of GDAX Feed: {length}
                 </div>
             </div>
         );
